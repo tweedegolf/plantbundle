a5d681c0da72f665692aa62809303df8104f1c6c
Hugo <hugovandepol@gmail.com>
2015-04-15 15:32:41 +0200

    changed command

--- src/TweedeGolf/PlantBundle/Search/PlantTransformer.php
+++ src/TweedeGolf/PlantBundle/Search/PlantTransformer.php
@@ -1,63 +1,154 @@
 <?php
 
+namespace TweedeGolf\PlantBundle\Command;
 
-namespace TweedeGolf\PlantBundle\Search;
-
-use FOS\ElasticaBundle\Transformer\ElasticaToModelTransformerInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use \Elastica\Client;
+use \Elastica\Document;
+use TweedeGolf\PlantBundle\Retriever\PlantRetriever;
 
 /**
- * Custom transformer needed for the findPaginated call in the PlantFinder
+ * Command for updating the tweedegolf 'plant' search index
  *
- * Class PlantTransformer
- * @package Tjt\MainBundle\Retriever
+ * Class ElasticaCommand
+ * @package TweedeGolf\PlantBundle\Command
  */
-class PlantTransformer implements ElasticaToModelTransformerInterface
+class ElasticaCommand extends ContainerAwareCommand
 {
-    /**
-     * Only get the ids form the search results
-     * @param array $elasticaObjects
-     * @return array
-     */
-    function transform(array $elasticaObjects)
+    private $derivedProperties = [
+        'edible',
+        'sustainable'
+    ];
+
+    protected function configure()
     {
-        $ids = [];
-        /** @var \Elastica\Result $obj */
-        foreach($elasticaObjects as $obj) {
-            $ids[] = $obj->getParam('_id');
+        $this
+            ->setName('elastica:refresh')
+            ->setDescription('Refresh the index');
+    }
+
+    protected function createIndex($index)
+    {
+        // Create the index new
+        $index->create(
+            array(
+                'number_of_shards' => 4,
+                'number_of_replicas' => 1,
+                'analysis' => array(
+                    'analyzer' => array(
+                        'indexAnalyzer' => array(
+                            'type' => 'custom',
+                            'tokenizer' => 'standard',
+                            'filter' => array('lowercase', 'mySnowball')
+                        ),
+                        'searchAnalyzer' => array(
+                            'type' => 'custom',
+                            'tokenizer' => 'standard',
+                            'filter' => array('standard', 'lowercase', 'mySnowball')
+                        )
+                    ),
+                    'filter' => array(
+                        'mySnowball' => array(
+                            'type' => 'snowball',
+                            'language' => 'Dutch'
+                        )
+                    )
+                )
+            ),
+            true
+        );
+    }
+
+    /* Execute: what happens when the command is executed */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var PlantRetriever $retriever */
+        $retriever = $this->getContainer()->get('tweedegolf_plant.plant_retriever');
+        $progress = $this->getHelperSet()->get('progress');
+
+        $port = $this->getContainer()->getParameter('tweedegolf_plant.elastica_port');
+        $host = $this->getContainer()->getParameter('tweedegolf_plant.elastica_host');
+
+        /* Elastica */
+        $client = new Client(['host' => $host, 'port' => $port]);
+        $index = $client->getIndex('plant');
+        $this->createIndex($index);
+        $type = $index->getType('plant');
+
+        $plantCount = $retriever->getPlantCount();
+        $progress->start($output, $plantCount);
+
+        $languages = $this->getContainer()->getParameter('languages');
+
+        $j = 0;
+        foreach($languages as $locale => $label) {
+            for ($i = 0; $i < $plantCount; $i += 100) {
+                $plants = $retriever->getLimitedPlants(100, $i, $locale);
+
+                foreach ($plants as $properties) {
+
+                    if (count($properties) > 0) {
+
+                        /* Fill a dummy entity with names, use */
+                        $document = [];
+                        $document['plantid'] = $properties[0]['plant_id'];
+                        $document['id'] = $j;
+                        $document['name'] = json_decode($properties[0]['names']);
+                        $document['locale'] = $locale;
+
+                        // set properties that have a value based only on their own 'values' key only
+                        foreach ($properties as $prop) {
+                            if (!in_array($prop['name'], $this->derivedProperties)) {
+                                $document[$prop['name']] = json_decode($prop['values']);
+                            }
+                        }
+
+                        // set derived properties
+                        $document['edible'] = $this->getEdibility($properties);
+                        $document['sustainable'] = $this->getSustainable($properties);
+
+                        $doc = new Document($j, $document);
+                        $type->addDocument($doc);
+                        $type->getIndex()->refresh();
+                        $progress->advance();
+                        $j += 1;
+                    }
+                }
+            }
         }
 
-        return $ids;
-
+        $progress->finish();
     }
 
     /**
-     * Not used
-     *
-     * @param array $elasticaObjects
-     * @return array
+     * @param $properties
+     * @return bool
      */
-    function hybridTransform(array $elasticaObjects)
+    private function getEdibility($properties)
     {
-        return [];
+        if (!isset($properties['fruit'])) {
+            return false;
+        }
+
+        $values = $properties['fruit'];
+
+        return in_array('edible', $values) || in_array('unusual taste', $values);
     }
 
     /**
-     * Not used
-     *
-     * @return string
+     * @param $properties
+     * @return bool
      */
-    function getObjectClass()
+    private function getSustainable($properties)
     {
-        return '';
-    }
+        if (!isset($properties['use'])) {
+            return false;
+        }
 
-    /**
-     * Not used
-     *
-     * @return string the identifier field
-     */
-    function getIdentifierField()
-    {
-        return '';
+        $use = $properties['use'];
+
+        return in_array('butterfly host plant', $use) || in_array('bee plant', $use);
     }
 }
